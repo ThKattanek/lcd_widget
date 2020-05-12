d24e311424b4de2484f4ae757b0911a5a66b65ad
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2020-05-12 06:11:05 +0200

    added lcd font roms

---
+++ lcd_widget.h
@@ -0,0 +1,27 @@
+#ifndef LCDWIDGET_H
+#define LCDWIDGET_H
+
+#include <QWidget>
+
+#define CHAR_WIDTH_PX         5
+#define CHAR_HEIGHT_PX        8
+
+#define CGRAM_STORAGE_CHARS  16
+#define ROM_FONT_CHARS       (256 - CGRAM_STORAGE_CHARS)
+
+class LCDWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit LCDWidget(QWidget *parent = nullptr);
+
+private:
+    /* font roms */
+    static const uint8_t fontA00[ROM_FONT_CHARS][CHAR_WIDTH_PX];
+    static const uint8_t fontA02[ROM_FONT_CHARS][CHAR_WIDTH_PX];
+
+signals:
+
+};
+
+#endif // LCDWIDGET_H
